c3f951a3df792d0855b4fcf62710bce9103f81d5
Gowtham Rao <rao@ohdsi.org>
2025-04-30 08:56:40 -0400

    Update test_pubmed.py

--- tests/test_pubmed.py
+++ tests/test_pubmed.py
@@ -15,6 +15,7 @@ import pytest
 import pandas as pd
 from pandas.testing import assert_frame_equal
 import requests
+from searchpubmed.pubmed import get_pmc_licenses
 
 # --------------------------------------------------------------------------- #
 # Helpers                                                                     #
